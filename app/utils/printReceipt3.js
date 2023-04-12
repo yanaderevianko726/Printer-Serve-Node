ff980d711cd99eff303cd54b37cf9130449327e8
Yana DEREVIANKO <79232031+yanaderevianko726@users.noreply.github.com>
2023-04-12 14:42:57 +0700

    update printer func

--- app/utils/printReceipt3.js
+++ app/utils/printReceipt3.js
@@ -109,7 +109,7 @@ var edgePrintReceipt3 = edge.func(function () {/*
             PrintString(new StringBuilder("EMOS-3"), 0);
             PrintFeedline(1);
 
-            SetBold(0);
+            SetBold(1);
 
             PrintString(new StringBuilder("PMKEY                 : 1234"), 0);
             PrintString(new StringBuilder("Rank                  : Pvt"), 0);
@@ -152,6 +152,8 @@ var edgePrintReceipt3 = edge.func(function () {/*
             PrintString(new StringBuilder("Phone: 0438 710 603"), 0);    
             PrintString(new StringBuilder("   "), 0);    
             PrintString(new StringBuilder("   "), 0);
+
+            SetBold(1);
 
             return 1;
         }
