7b1d3513f73f64c06606d389014c012ec4b574ee
Yana DEREVIANKO <79232031+yanaderevianko726@users.noreply.github.com>
2023-04-17 07:28:28 +0700

    update rfid module

--- app/utils/writeRfid.js
+++ app/utils/writeRfid.js
@@ -1,4 +1,7 @@
-var edge = require('edge-js');
+var ffi = require('ffi-napi');
+var ndpi = new ffi.Library('K720_Dll.dll', {
+    K720_CommOpen: ['void', ['char']],
+});
 
 // constructor
 const WriteRFID = function (obj) {
@@ -6,7 +9,13 @@ const WriteRFID = function (obj) {
 };
 
 WriteRFID.writeRfidCard = (pdfs, rfidModel, result) => {
+    ndpi.K720_CommOpen();
     result(null, { retInt: 0, ...rfidModel });
+    // WriteCrapVB('1234', function (error, retVal) {
+    //     if (error) throw error;
+    //     console.log(retVal);
+    //     result(null, { retInt: retVal, ...rfidModel });
+    // });
 };
 
 module.exports = WriteRFID;
