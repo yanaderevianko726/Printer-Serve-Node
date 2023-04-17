a2d069413f7fdc3bea709de26d732c577b733c4c
Yana DEREVIANKO <79232031+yanaderevianko726@users.noreply.github.com>
2023-04-17 10:42:35 +0700

    update comport module

--- app/controllers/rfid.controller.js
+++ app/controllers/rfid.controller.js
@@ -18,6 +18,25 @@ exports.initController = (req, res) => {
 };
 
 // Create and Save a new Pdf
+exports.openComPort = (req, res) => {
+  // Validate request
+  if (!req.body) {
+    res.status(400).send({
+      message: "Content can not be empty!"
+    });
+  }
+
+  WriteRFID.openComPort(rfidModel, (err, data) => {
+    if (err)
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the Pdf."
+      });
+    else res.send(data);
+  });
+};
+
+// Create and Save a new Pdf
 exports.writteRfid = (req, res) => {
   // Validate request
   if (!req.body) {
--- app/utils/writeRfid.js
+++ app/utils/writeRfid.js
@@ -1,6 +1,8 @@
-var SerialPort = require("serialport").SerialPort
-var comPort1 = new SerialPort('COM1', {
-    baudRate: 9600
+const { SerialPort } = require('serialport')
+var comPort1 = new SerialPort({
+    path: 'COM1',
+    baudRate: 9600,
+    autoOpen: false,
 });
 
 // constructor
@@ -8,21 +10,26 @@ const WriteRFID = function (obj) {
 
 };
 
-WriteRFID.openComPort = (pdfs, rfidModel, result) => {
-    comPort1.on("open", function () {
-        console.log("-- Com1 opened with 9600 baudRate--");
-    });
+WriteRFID.openComPort = (rfidModel, result) => {
+    comPort1.open(function (err) {
+        if (err) {
+            console.log("-- Com1 openfailed --");
+            result(null, { retInt: 1, ...rfidModel });
+        } else {
+            console.log("-- Com1 opened with 9600 baudRate. --");
+            result(null, { retInt: 0, ...rfidModel });
+        }
+    })
 };
 
 WriteRFID.writeRfidCard = (pdfs, rfidModel, result) => {
     comPort1.write('023030000141500312', function (err) {
         if (err) {
             result(null, { retInt: 1, ...rfidModel });
+        } else {
+            console.log("-- Com1 wrote with 023030000141500312 --");
+            result(null, { retInt: 0, ...rfidModel });
         }
-    });
-    comPort1.on("data", function (data) {
-        console.log("Data received: " + data);
-        result(null, { retInt: data, ...rfidModel });
     });
 };
 
