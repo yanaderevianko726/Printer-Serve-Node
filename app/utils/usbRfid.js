9103f471f39b4a12900a0c74b36aee23da36bda3
Yana DEREVIANKO <79232031+yanaderevianko726@users.noreply.github.com>
2023-04-18 05:24:54 +0700

    add usb rfid reader

--- app/utils/usbRfid.js
+++ app/utils/usbRfid.js
@@ -1,4 +1,38 @@
 var edge = require('edge-js');
+
+var edgeReadInfo = edge.func(function () {/*
+    using System.Threading.Tasks;
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Drawing;
+    using System.Linq;
+    using System.Text;
+    using System.Runtime.InteropServices;
+    using System.IO.Ports;
+    using System.Reflection;
+    using System.Text.RegularExpressions;
+    using System.Runtime.CompilerServices;
+    using System.Diagnostics;
+    using System.Threading;
+
+    public class Startup
+    {
+        [DllImport("USB.dll", EntryPoint = "GetSerNum", CharSet = CharSet.Ansi)]
+        public static extern int GetSerNum(StringBuilder buffer);
+
+        string strBuffer = "Buffer";
+        int r = -1;
+
+        public async Task<object> Invoke(dynamic input)
+        {
+            StringBuilder buffer = new StringBuilder(strBuffer, strBuffer.Length);
+            r = GetSerNum(buffer);
+            return r;
+        }
+    }
+*/});
+
 var edgeWritePmKey = edge.func(function () {/*
     using System.Threading.Tasks;
     using System;
@@ -17,8 +51,8 @@ var edgeWritePmKey = edge.func(function () {/*
 
     public class Startup
     {
-        [DllImport("Msprintsdk.dll", EntryPoint = "SetInit", CharSet = CharSet.Ansi)]
-        public static extern int SetInit();
+        [DllImport("USB.dll", EntryPoint = "GetSerNum", CharSet = CharSet.Ansi)]
+        public static extern int GetSerNum(StringBuilder buffer);
 
         int m_iInit = -1;
 
@@ -35,7 +69,7 @@ const UsbRfid = function (obj) {
 };
 
 UsbRfid.readInfo = (reqBody, result) => {
-    edgeWritePmKey(reqBody, function (error, retVal) {
+    edgeReadInfo(reqBody, function (error, retVal) {
         if (error) throw error;
         console.log(retVal);
         result(null, { retInt: retVal, ...reqBody });
