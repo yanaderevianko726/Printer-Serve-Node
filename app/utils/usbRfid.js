68500ec5f43d27e896bb0385f143df7f9bb05009
Yana DEREVIANKO <79232031+yanaderevianko726@users.noreply.github.com>
2023-04-18 13:57:04 +0700

    update pmkey writte

--- app/utils/usbRfid.js
+++ app/utils/usbRfid.js
@@ -18,14 +18,38 @@ var edgeReadInfo = edge.func(function () {/*
 
     public class Startup
     {
-        [DllImport("function.dll", EntryPoint = "GetSerNum", CharSet = CharSet.Ansi)]
-        public static extern int GetSerNum(byte[] buffer);
+        [DllImport("function.dll")]
+        public static extern int UL_HLRead(byte mode, byte blk_add, [In]byte[] snr, [In]byte[] buffer);
+
+        private string showData(byte[] data, int s, int e)
+        {
+            string txt = "";
+            for (int i = 0; i < e; i++) {
+                if (data[s + i] < 0)
+                    data[s + i] = Convert.ToByte(Convert.ToInt32(data[s + i]) + 256);
+            }
+
+            for (int i = 0; i < e; i++) {
+                txt += data[s + i].ToString("X2")+" ";
+            }
+            return txt;
+        }
 
         public async Task<object> Invoke(dynamic input)
         {
-            byte[] buffer = new byte[9];
-            int nRet = GetSerNum(buffer);
-            return nRet;
+            byte mode = (byte)0x00;
+            byte blk_add = Convert.ToByte("04", 16);
+
+            byte[] snr = new byte[7];
+            byte[] buffer = new byte[16];
+
+            int nRet = UL_HLRead(mode, blk_add, snr, buffer);
+            if (nRet != 0) {
+                return "";
+            } else {
+                string bufferData = showData(buffer, 0, 16);
+                return bufferData;
+            }
         }
     }
 */});
@@ -41,21 +65,71 @@ var edgeWritePmKey = edge.func(function () {/*
     using System.Runtime.InteropServices;
     using System.IO.Ports;
     using System.Reflection;
-    using System.Text.RegularExpressions;
     using System.Runtime.CompilerServices;
     using System.Diagnostics;
     using System.Threading;
+    using System.Text.RegularExpressions;
 
     public class Startup
     {
-        [DllImport("function.dll", EntryPoint = "GetSerNum", CharSet = CharSet.Ansi)]
-        public static extern int GetSerNum(StringBuilder buffer);
+        [DllImport("function.dll")]
+        public static extern int UL_HLWrite(byte mode, byte blk_add, [In]byte[] snr, [In]byte[] buffer);
 
-        int m_iInit = -1;
+        private string formatStr(string str, int num_blk)
+        {            
+            string tmp=Regex.Replace(str,"[^a-fA-F0-9]","");
+            if (num_blk == -1) return tmp;
+            if (num_blk < -1) {
+                if (tmp.Length != -16 / num_blk * 2) return null;
+                else return tmp;
+            }
+            if (tmp.Length != 16*num_blk*2) return null;
+            else return tmp;
+        }
+
+        private void convertStr(byte[] after, string before, int length)
+        {
+            for (int i = 0; i < length; i++) {
+                after[i] = Convert.ToByte(before.Substring(2 * i, 2), 16);
+            }
+        }
+
+        private string showData(byte[] data, int s, int e)
+        {
+            string txt = "";
+            for (int i = 0; i < e; i++) {
+                if (data[s + i] < 0)
+                    data[s + i] = Convert.ToByte(Convert.ToInt32(data[s + i]) + 256);
+            }
+
+            for (int i = 0; i < e; i++) {
+                txt += data[s + i].ToString("X2")+" ";
+            }
+            return txt;
+        }
 
         public async Task<object> Invoke(dynamic input)
         {
-            return 1;
+            string writteData = input.pmKey;
+            byte mode = 0x00;
+            byte blk_add = Convert.ToByte("04", 16);
+
+            byte[] snr = new byte[7] { 0, 0, 0, 0, 0, 0, 0 };
+            byte[] buffer = new byte[4];
+
+            string bufferStr = formatStr(writteData, -1);
+            convertStr(buffer, bufferStr, 4);
+
+            int nRet = UL_HLWrite(mode, blk_add, snr, buffer);
+            string cardNum = "";
+
+            if (nRet != 0) {
+                return "";
+            }
+            else {
+                cardNum = showData(snr,0,7);
+                return cardNum;
+            }
         }
     }
 */});
@@ -69,7 +143,7 @@ UsbRfid.readInfo = (reqBody, result) => {
     edgeReadInfo(reqBody, function (error, retVal) {
         if (error) throw error;
         console.log(retVal);
-        result(null, { retInt: retVal, ...reqBody });
+        result(null, { serNum: retVal });
     });
 };
 
