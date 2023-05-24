1a94b1c33c89bc813ad45933514f219e37d1e929
Yana DEREVIANKO <79232031+yanaderevianko726@users.noreply.github.com>
2023-05-24 15:57:45 +0700

    update com port control

--- app/utils/writeRfid.js
+++ app/utils/writeRfid.js
@@ -9,25 +9,22 @@ var comPort1 = new SerialPort({
 var res2 = '';
 
 comPort1.on('data', function (data) {
-    console.log('--- Com1 received data:', data)
     let currentRes = data.toString();
     if (currentRes == Buffer.from([0x06, 0x30, 0x30]).toString()) {
-        console.log('--- response equals with : 06 30 30')
-        res2 = '';
+        console.log('--- Com1 received data: 06 30 30');
 
+        res2 = '';
         let cmd1 = Buffer.from([0x05, 0x30, 0x30]);
         comPort1.write(cmd1, function (err) {
-            if (!err) {
-                console.log("-- Com1 wrote with : 05 30 30");
-            }
+            console.log("-- Com1 wrote with : 05 30 30");
         });
     } else if (currentRes.includes("03")) {
+        console.log('--- Com1 received data:', data.toString());
+
         res2 = '';
         let cmd = Buffer.from([0x02, 0x30, 0x30, 0x00, 0x02, 0x41, 0x50, 0x03, 0x12]);
         comPort1.write(cmd, function (err) {
-            if (!err) {
-                console.log("-- Com1 wrote with : 02 30 30 00 02 41 50 03 12");
-            }
+            console.log("-- Com1 wrote with : 02 30 30 00 02 41 50 03 12");
         });
     } else {
         res2 = res2 + ' ' + currentRes;
@@ -41,20 +38,19 @@ const WriteRFID = function (obj) {
 };
 
 WriteRFID.openCom1Port = (rfidModel, result) => {
+    res2 = '';  
     comPort1.open(function (err) {
         if (err) {
-            console.log("-- Com1 openfailed --");
+            console.log("-- Com1 open failed --");
             result(null, { retInt: 1, ...rfidModel });
         } else {
             console.log("-- Com1 opened with 9600 baudRate. --");
-            res2 = '';
             let cmd = Buffer.from([0x02, 0x30, 0x30, 0x00, 0x02, 0x41, 0x50, 0x03, 0x12])
             comPort1.write(cmd, function (err) {
+                console.log("-- Com1 wrote with: 02 30 30 00 02 41 50 03 12");
                 if (err) {
-                    console.log("-- Com1 write error ---");
                     result(null, { retInt: 1, ...rfidModel });
                 } else {
-                    console.log("-- Com1 wrote with: 02 30 30 00 02 41 50 03 12");
                     result(null, { retInt: 0, ...rfidModel });
                 }
             });
@@ -66,10 +62,10 @@ WriteRFID.sendCardToReadPos = (rfidModel, result) => {
     res2 = '';
     let cmd = Buffer.from([0x02, 0x30, 0x30, 0x00, 0x03, 0x46, 0x43, 0x37, 0x03, 0x30])
     comPort1.write(cmd, function (err) {
+        console.log("-- Com1 wrote with: 02 30 30 00 03 46 43 37 03 30");
         if (err) {
             result(null, { retInt: 1, ...rfidModel });
         } else {
-            console.log("-- Com1 wrote with: 02 30 30 00 03 46 43 37 03 30");
             result(null, { retInt: 0, ...rfidModel });
         }
     });
@@ -79,10 +75,10 @@ WriteRFID.sendCardToTakePos = (rfidModel, result) => {
     res2 = '';
     let cmd = Buffer.from([0x02, 0x30, 0x30, 0x00, 0x03, 0x46, 0x43, 0x34, 0x03, 0x33])
     comPort1.write(cmd, function (err) {
+        console.log("-- Com1 wrote with: 02 30 30 00 03 46 43 34 03 33");
         if (err) {
             result(null, { retInt: 1, ...rfidModel });
         } else {
-            console.log("-- Com1 wrote with: 02 30 30 00 03 46 43 34 03 33");
             result(null, { retInt: 0, ...rfidModel });
         }
     });
@@ -92,10 +88,10 @@ WriteRFID.sendCardToOutPos = (rfidModel, result) => {
     res2 = '';
     let cmd = Buffer.from([0x02, 0x30, 0x30, 0x00, 0x03, 0x46, 0x43, 0x30, 0x03, 0x37])
     comPort1.write(cmd, function (err) {
+        console.log("-- Com1 wrote with: 02 30 30 00 03 46 43 30 03 37");
         if (err) {
             result(null, { retInt: 1, ...rfidModel });
         } else {
-            console.log("-- Com1 wrote with: 02 30 30 00 03 46 43 30 03 37");
             result(null, { retInt: 0, ...rfidModel });
         }
     });
