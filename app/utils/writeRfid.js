0b1554d14ffe2b33b783933e35e94705c60a5e19
Yana DEREVIANKO <79232031+yanaderevianko726@users.noreply.github.com>
2023-05-29 04:29:39 +0700

    update comport apis

--- app/utils/writeRfid.js
+++ app/utils/writeRfid.js
@@ -1,36 +1,4 @@
-const { SerialPort } = require('serialport')
-
-var comPort1 = new SerialPort({
-    path: 'COM1',
-    baudRate: 9600,
-    autoOpen: false,
-});
-
-var res2 = '';
-
-comPort1.on('data', function (data) {
-    let currentRes = data.toString();
-    if (currentRes == Buffer.from([0x06, 0x30, 0x30]).toString()) {
-        console.log('--- Com1 received data: 06 30 30');
-
-        res2 = '';
-        let cmd1 = Buffer.from([0x05, 0x30, 0x30]);
-        comPort1.write(cmd1, function (err) {
-            console.log("-- Com1 wrote with : 05 30 30");
-        });
-    } else if (currentRes.includes("03")) {
-        console.log('--- Com1 received data:', data.toString());
-
-        res2 = '';
-        let cmd = Buffer.from([0x02, 0x30, 0x30, 0x00, 0x02, 0x41, 0x50, 0x03, 0x12]);
-        comPort1.write(cmd, function (err) {
-            console.log("-- Com1 wrote with : 02 30 30 00 02 41 50 03 12");
-        });
-    } else {
-        res2 = res2 + ' ' + currentRes;
-        console.log("-- res2 : ", res2);
-    }
-});
+var comPort1 = require("../server.js");
 
 // constructor
 const WriteRFID = function (obj) {
@@ -38,7 +6,6 @@ const WriteRFID = function (obj) {
 };
 
 WriteRFID.openCom1Port = (rfidModel, result) => {
-    res2 = '';  
     comPort1.open(function (err) {
         if (err) {
             console.log("-- Com1 open failed --");
@@ -59,7 +26,6 @@ WriteRFID.openCom1Port = (rfidModel, result) => {
 };
 
 WriteRFID.sendCardToReadPos = (rfidModel, result) => {
-    res2 = '';
     let cmd = Buffer.from([0x02, 0x30, 0x30, 0x00, 0x03, 0x46, 0x43, 0x37, 0x03, 0x30])
     comPort1.write(cmd, function (err) {
         console.log("-- Com1 wrote with: 02 30 30 00 03 46 43 37 03 30");
@@ -72,7 +38,6 @@ WriteRFID.sendCardToReadPos = (rfidModel, result) => {
 };
 
 WriteRFID.sendCardToTakePos = (rfidModel, result) => {
-    res2 = '';
     let cmd = Buffer.from([0x02, 0x30, 0x30, 0x00, 0x03, 0x46, 0x43, 0x34, 0x03, 0x33])
     comPort1.write(cmd, function (err) {
         console.log("-- Com1 wrote with: 02 30 30 00 03 46 43 34 03 33");
@@ -85,7 +50,6 @@ WriteRFID.sendCardToTakePos = (rfidModel, result) => {
 };
 
 WriteRFID.sendCardToOutPos = (rfidModel, result) => {
-    res2 = '';
     let cmd = Buffer.from([0x02, 0x30, 0x30, 0x00, 0x03, 0x46, 0x43, 0x30, 0x03, 0x37])
     comPort1.write(cmd, function (err) {
         console.log("-- Com1 wrote with: 02 30 30 00 03 46 43 30 03 37");
--- server.js
+++ server.js
@@ -1,22 +1,40 @@
 const express = require("express");
 const cors = require("cors");
+const { SerialPort } = require('serialport')
 
 const app = express();
 const PORT = process.env.PORT || 8081;
 
-var corsOptions = {
+module.exports const comPort1 = new SerialPort({
+    path: 'COM1',
+    baudRate: 9600,
+    autoOpen: false,
+});
+
+comPort1.on('data', function (data) {
+    let currentRes = data.toString();
+    if (currentRes == Buffer.from([0x06, 0x30, 0x30]).toString()) {
+        console.log('--- Com1 received data: 06 30 30');
+
+        let cmd1 = Buffer.from([0x05, 0x30, 0x30]);
+        comPort1.write(cmd1, function (err) {
+            console.log("-- Com1 wrote with : 05 30 30");
+        });
+    } else if (currentRes.includes("03")) {
+        console.log('--- Com1 received data:', data.toString());
+
+        let cmd = Buffer.from([0x02, 0x30, 0x30, 0x00, 0x02, 0x41, 0x50, 0x03, 0x12]);
+        comPort1.write(cmd, function (err) {
+            console.log("-- Com1 wrote with : 02 30 30 00 02 41 50 03 12");
+        });
+    }
+});
+
+app.use(cors({
   origin: "http://localhost:8001"
-};
-
-app.use(cors(corsOptions));
-
-// parse requests of content-type - application/json
+}));
 app.use(express.json());
-
-// parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
-
-// simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to our application." });
 });
@@ -27,5 +45,16 @@ require("./app/routes/usbrfid.routes.js")(app);
 require("./app/routes/user.routes.js")(app);
 
 app.listen(PORT, () => {
+  comPort1.open(function (err1) {
+      if (err1) {
+          console.log("-- Com1 open failed --");
+      } else {
+          console.log("-- Com1 opened with 9600 baudRate. --");
+          let cmd = Buffer.from([0x02, 0x30, 0x30, 0x00, 0x02, 0x41, 0x50, 0x03, 0x12])
+          comPort1.write(cmd, function (err) {
+              console.log("-- Com1 wrote with: 02 30 30 00 02 41 50 03 12");
+          });
+      }
+  })
   console.log(`Server is running on port ${PORT}.`);
 });
